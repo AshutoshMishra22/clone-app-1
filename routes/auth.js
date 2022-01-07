56e6641b685c149d55f19efce3f6a3f66ceeb7b6
ashutosh001 <63530972+AshutoshMishra22@users.noreply.github.com>
2022-01-07 14:59:22 +0530

    change

--- routes/auth.js
+++ routes/auth.js
@@ -37,7 +37,6 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(401).json("Wrong Password or Username !");
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
-    console.log("Pasword ", bytes);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong Password or Username !");
